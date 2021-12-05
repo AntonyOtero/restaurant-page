29bb929ac1e3146f712f42b7929cd8b8e26c917f
Antony Otero <antonyotero.nyc@gmail.com>
2021-12-05 15:10:36 -0500

    Setup Contact module directory

---
+++ src/contact/contact.js
@@ -0,0 +1,11 @@
+import './contact.css'
+
+const contact = () => {
+  const container = document.createElement('div')
+
+  container.classList.add('contact')
+
+  return container
+}
+
+export {contact}
